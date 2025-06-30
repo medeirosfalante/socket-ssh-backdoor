38686f545ce3339710d33d58d939a91274c22f27
Rafael Medeiros <rafaelgurudesign@gmail.com>
2025-06-29 22:59:14 -0300

    receive json infos

--- serverssh.c
+++ serverssh.c
@@ -14,9 +14,7 @@
 
 #include <cjson/cJSON.h>  // lib cJSON
 
-#define PORT 4444
-
-
+#define PORT 8080
 
 // Função para decodificar base64
 unsigned char *base64_decode(const char *input, int *len) {
@@ -35,9 +33,6 @@ unsigned char *base64_decode(const char *input, int *len) {
     return buffer;
 }
 
-
-
-
 // Executa o comando recebido e envia de volta a saída
 void execute_command(const char *cmd, int client_sock) {
     FILE *fp = popen(cmd, "r");
@@ -55,102 +50,39 @@ void execute_command(const char *cmd, int client_sock) {
     pclose(fp);
 }
 
-
-
-// Chave e IV fixos em hexadecimal (AES-256-CBC)
-#define KEY_HEX "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
-#define IV_HEX  "0102030405060708090a0b0c0d0e0f10"
-
 int main() {
-    int server_sock, client_sock;
+    int sockfd, client_sock;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    char buffer[2048];
-    char command[4096];
-    char output[2048];
-    FILE *fp;
+    char buffer[4096] = {0};
 
-    // Criação do socket
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
-        perror("Erro ao criar socket");
-        exit(1);
-    }
-
-    // Configura o endereço do servidor
+    // Cria socket TCP
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // Faz o bind
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Erro no bind");
-        close(server_sock);
-        exit(1);
-    }
+    bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(sockfd, 1);
 
-    // Escuta conexões
-    listen(server_sock, 1);
     printf("Servidor escutando na porta %d...\n", PORT);
 
-    // Aceita conexão
-    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
-    if (client_sock < 0) {
-        perror("Erro no accept");
-        close(server_sock);
-        exit(1);
+    client_sock = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+    printf("Cliente conectado!\n");
+
+    // Recebe JSON
+    recv(client_sock, buffer, sizeof(buffer), 0);
+
+    // Parse do JSON recebido
+    cJSON *json = cJSON_Parse(buffer);
+    if (!json) {
+        fprintf(stderr, "Erro ao parsear JSON\n");
+        close(client_sock);
+        return 1;
     }
 
-    printf("Conexão recebida de %s\n", inet_ntoa(client_addr.sin_addr));
+    const char *keyblob_b64 = cJSON_GetObjectItem(json, "keyblob")->valuestring;
+    const char *payload_b64 = cJSON_GetObjectItem(json, "payload")->valuestring;
 
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int n = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
-        if (n <= 0) break;
-
-        buffer[n] = '\0';
-
-        // Comando openssl para decifrar
-        snprintf(command, sizeof(command),
-            "echo '%s' | openssl enc -aes-256-cbc -a -d -K %s -iv %s 2>/dev/null",
-            buffer, KEY_HEX, IV_HEX);
-
-        // Executa a decifragem
-        fp = popen(command, "r");
-        if (!fp) {
-            char *msg = "Erro ao decifrar comando\n";
-            send(client_sock, msg, strlen(msg), 0);
-            continue;
-        }
-
-        // Lê o comando decifrado
-        char decrypted[1024];
-        if (fgets(decrypted, sizeof(decrypted), fp) == NULL) {
-            char *msg = "Falha na leitura do comando\n";
-            send(client_sock, msg, strlen(msg), 0);
-            pclose(fp);
-            continue;
-        }
-        decrypted[strcspn(decrypted, "\n")] = 0; // Remove newline
-        pclose(fp);
-
-        // Executa o comando real
-        fp = popen(decrypted, "r");
-        if (!fp) {
-            char *msg = "Erro ao executar comando\n";
-            send(client_sock, msg, strlen(msg), 0);
-            continue;
-        }
-
-        // Envia saída do comando
-        while (fgets(output, sizeof(output), fp)) {
-            send(client_sock, output, strlen(output), 0);
-        }
-
-        pclose(fp);
-    }
-
-    close(client_sock);
-    close(server_sock);
     return 0;
 }
